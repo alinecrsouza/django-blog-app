ecde6200e787e7e287fb88d5ae882167b6e721b6
alinecrsouza <linecrsouza@gmail.com.br>
2016-10-05 13:03:32 -0300

    Creating and Running Migrations

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,24 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Category
+
 
 def home(request):
     #return HttpResponse("Hello World")
 
     name = "Aline"
-    languages = {'Python', 'PHP', 'Java', 'Ruby'}
+    categories = {'PHP', 'Java', 'Ruby'}
+    for category in categories:
+        Category.objects.create(name=category)
+
+    all_categories = Category.objects.all()
 
     context = {
         'name': name,
-        'languages':languages
+        'categories': all_categories
     }
+
+    #Category.objects.destroy(id=1)
 
     return render(request, 'blog/home.html', context)
