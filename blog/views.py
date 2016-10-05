3a3dc637a84be800bf01681da66bf73ca4e5f1df
alinecrsouza <linecrsouza@gmail.com.br>
2016-10-05 11:57:58 -0300

    Sending parameters to the view | Creating the Category model

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World")
+    #return HttpResponse("Hello World")
+
+    name = "Aline"
+    languages = {'Python', 'PHP', 'Java', 'Ruby'}
+
+    context = {
+        'name': name,
+        'languages':languages
+    }
+
+    return render(request, 'blog/home.html', context)
