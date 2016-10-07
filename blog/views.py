d6fe6a04e64f70de926a0b1edf0f3cce27ceb602
alinecrsouza <linecrsouza@gmail.com.br>
2016-10-07 11:44:39 -0300

    Creating view posts by category

--- blog/views.py
+++ blog/views.py
@@ -9,14 +9,14 @@ def home(request):
     #return HttpResponse("Hello World")
 
     name = "Aline"
-    #categories = {'PHP', 'Java', 'Ruby'}
-    #for category in categories:
+    # categories = {'PHP', 'Java', 'Ruby'}
+    # for category in categories:
     #    Category.objects.create(name=category)
 
     all_categories = Category.objects.all()
 
     # category_python = Category.objects.get(id = 5)
-    #posts = Post.objects.all()
+    # posts = Post.objects.all()
     posts = Post.objects.filter(status='Published')
 
     # post = Post()
@@ -35,4 +35,15 @@ def home(request):
 
     #Category.objects.destroy(id=1)
 
+    return render(request, 'blog/home.html', context)
+
+def show_posts_by_category(request, category_id):
+    all_categories = Category.objects.all()
+    category = Category.objects.get(pk = category_id)
+    posts = Post.objects.filter(category = category, status = 'Published')
+    context ={
+        'posts': posts,
+        'categories': all_categories,
+        'category': category,
+    }
     return render(request, 'blog/home.html', context)
